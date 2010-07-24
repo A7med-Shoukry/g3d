385fb3d5786d6c07dff594a16088ff638107112c
corey_taylor <corey_taylor@ef0428c2-0c16-0410-9f3f-d710ca1f8061>
2010-07-24 04:47:56 +0000

    Cleaning out SVN repository.
    git-svn-id: https://g3d.svn.sourceforge.net/svnroot/g3d@15 ef0428c2-0c16-0410-9f3f-d710ca1f8061

--- G3D9-morgan/GLG3D.lib/include/GLG3D/Camera.h
+++
@@ -1,9 +0,0 @@
-class Camera {
-public:
-    enum Type {ORTHOGRAPHIC, PERSPECTIVE, CUSTOM};
-
-    /** Camera-to-world matrix */
-    const CFrame& cframe() const;
-    const CFrame&  worldToCameraMatrix() const;
-    const Matrix4& projectionMatrix(int screenWidth, int screenHeight) const;
-};
