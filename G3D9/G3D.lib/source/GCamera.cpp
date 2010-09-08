9ec1f63e3073a19434093e4ab510187a1157886c
morgan3d <morgan3d@ef0428c2-0c16-0410-9f3f-d710ca1f8061>
2010-09-08 04:03:40 +0000

    git-svn-id: https://g3d.svn.sourceforge.net/svnroot/g3d@142 ef0428c2-0c16-0410-9f3f-d710ca1f8061

--- G3D9/G3D.lib/source/GCamera.cpp
+++ G3D9/G3D.lib/source/GCamera.cpp
@@ -96,7 +96,7 @@ float GCamera::circleOfConfusionRadius(float z, const class Rect2D& viewport) co
 
 
 GCamera::GCamera() : m_lensRadius(0.0f), m_focusPlaneZ(-10.0f), m_exposureTime(0.0f) {
-    setNearPlaneZ(-0.2f);
+    setNearPlaneZ(-0.15f);
     setFarPlaneZ(-150.0f);
     setFieldOfView((float)toRadians(90.0f), HORIZONTAL);
 }
