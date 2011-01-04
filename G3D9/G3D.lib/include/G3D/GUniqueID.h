5d302fabe0ab4299f5e7d531405b5ad186cb88f0
morgan3d <morgan3d@ef0428c2-0c16-0410-9f3f-d710ca1f8061>
2011-01-04 22:01:01 +0000

    git-svn-id: https://g3d.svn.sourceforge.net/svnroot/g3d@426 ef0428c2-0c16-0410-9f3f-d710ca1f8061

--- G3D9/G3D.lib/include/G3D/GUniqueID.h
+++ G3D9/G3D.lib/include/G3D/GUniqueID.h
@@ -1,13 +1,14 @@
 /**
-  @file GUniqueID.h
-  @author Morgan McGuire, http://graphics.cs.williams.edu
+  \file G3D/GUniqueID.h
+  \author Morgan McGuire, http://graphics.cs.williams.edu
  */
-#ifndef G3D_GUNIQUEID_H
-#define G3D_GUNIQUEID_H
+#ifndef G3D_GUniqueID_h
+#define G3D_GUniqueID_h
 
 #include "G3D/platform.h"
 #include "G3D/g3dmath.h"
 #include "G3D/Table.h"
+#include "G3D/Any.h"
 
 namespace G3D {
 
@@ -27,6 +28,27 @@ public:
 
     GUniqueID() : id(0) {}
 
+    GUniqueID& operator=(const Any& a) {
+        a.verifyName("GUniqueID");
+        a.verifyType(Any::ARRAY);
+        a.verifySize(4);
+        id = (uint64(a[3].number()) << 48) | (uint64(a[2].number()) << 32) | (uint64(a[1].number()) << 16) | uint64(a[0].number());
+        return *this;
+    }
+
+    GUniqueID(const Any& a) {
+        *this = a;
+    }
+
+    Any toAny() const {
+        Any a(Any::ARRAY, "GUniqueID");
+        a.resize(4);
+        for (int i = 0; i < 4; ++i) {
+            a[i] = Any(float(uint16(id >> (i * 16))));
+        }
+        return a;
+    }
+
     bool uninitialized() const {
         return id == 0;
     }
@@ -37,7 +59,7 @@ public:
 
     operator uint64() const {
         return id;
-    }
+    }    
 
     bool operator==(const GUniqueID& other) const {
         return id == other.id;
