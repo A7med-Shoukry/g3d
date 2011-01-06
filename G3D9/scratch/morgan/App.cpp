537e2a74bd920ab7149803806893326f8b026078
morgan3d <morgan3d@ef0428c2-0c16-0410-9f3f-d710ca1f8061>
2011-01-06 02:26:28 +0000

    git-svn-id: https://g3d.svn.sourceforge.net/svnroot/g3d@435 ef0428c2-0c16-0410-9f3f-d710ca1f8061

--- G3D9/scratch/morgan/App.cpp
+++ G3D9/scratch/morgan/App.cpp
@@ -66,12 +66,6 @@ public:
 };
 
 int main(int argc, char** argv) {
-    GUniqueID i = GUniqueID::create(37);
-    std::string s = i.toString16();
-    debugPrintf("%s\n", s.c_str());
-    GUniqueID j = GUniqueID::fromString16(s);
-
-    return 0;
 #if 0
     Image1::Ref im = Image1::createEmpty(32, 64);
     for (int y = 0; y < im->height(); ++y) {
@@ -117,8 +111,104 @@ App::App(const GApp::Settings& settings) : GApp(settings) {
 #   endif
 }
 
+class ImageLoader : public GThread {
+public:
+    typedef ReferenceCountedPointer<ImageLoader> Ref;
 
+    const std::string filename;
+    GLuint  pbo;
+    GLvoid* ptr;
+    GImage  im;
+    int     sidePixels;
+
+    /** Must be invoked on the OpenGL thread */
+    ImageLoader(const std::string& filename, int sidePixels) : GThread(filename), filename(filename), sidePixels(sidePixels) {
+        glGenBuffers(1, &pbo);
+        glBindBuffer(GL_PIXEL_UNPACK_BUFFER, pbo);
+        size_t size = sidePixels * sidePixels * 3;
+        glBufferData(GL_PIXEL_UNPACK_BUFFER, size, NULL, GL_STREAM_DRAW);
+        ptr = glMapBuffer(GL_PIXEL_UNPACK_BUFFER, GL_WRITE_ONLY);
+        glBindBuffer(GL_PIXEL_UNPACK_BUFFER, GL_NONE);
+    }
+
+    virtual void threadMain() {
+        im.load(filename);
+        debugAssert(im.width() == sidePixels);
+        debugAssert(im.height() == sidePixels);
+        debugAssert(im.channels() == 3);
+        System::memcpy(ptr, im.byte(), im.width() * im.height() * im.channels());
+    }
+
+    /** Call on the GL thread after threadMain is done */
+    void unmap() {
+        glBindBuffer(GL_PIXEL_UNPACK_BUFFER, pbo);
+        glUnmapBuffer(GL_PIXEL_UNPACK_BUFFER);
+        glBindBuffer(GL_PIXEL_UNPACK_BUFFER, GL_NONE);
+        ptr = NULL;
+    }
+
+    /** Must be invoked on the OpenGL thread */
+    virtual ~ImageLoader() {
+        glDeleteBuffers(1, &pbo);
+        pbo = GL_NONE;
+    }
+};
+
+Texture::Ref sky;
 void App::onInit() {
+    Stopwatch stopwatch;
+    stopwatch.tick();
+
+    Texture::Settings settings = Texture::Settings::cubeMap();
+    settings.interpolateMode = Texture::BILINEAR_NO_MIPMAP;
+    sky = Texture::createEmpty("cubemap", 0, 0, ImageFormat::RGB8(), Texture::DIM_CUBE_MAP, settings);
+#if 0
+    // Using multiple threads cuts the load time from disk from 0.9s to 0.6s.
+    // Using PBO and multiple threads for DMA has no measurable impact on performance over single-threaded glTexImage2D from
+    // the CPU memory--both take about 1.5s.
+    {
+        ThreadSet threadSet;
+        int w = 2048;
+        Array<ImageLoader::Ref> image;
+        image.append(new ImageLoader("D:/morgan/g3d/data/cubemap/sky_skylab_01/sky_skylab_01bk.png", w));
+        image.append(new ImageLoader("D:/morgan/g3d/data/cubemap/sky_skylab_01/sky_skylab_01dn.png", w));
+        image.append(new ImageLoader("D:/morgan/g3d/data/cubemap/sky_skylab_01/sky_skylab_01lf.png", w));
+        image.append(new ImageLoader("D:/morgan/g3d/data/cubemap/sky_skylab_01/sky_skylab_01ft.png", w));
+        image.append(new ImageLoader("D:/morgan/g3d/data/cubemap/sky_skylab_01/sky_skylab_01rt.png", w));
+        image.append(new ImageLoader("D:/morgan/g3d/data/cubemap/sky_skylab_01/sky_skylab_01up.png", w));
+        for (int i = 0; i < 6; ++i) {
+            threadSet.insert(image[i]);
+        }
+
+        // Run the threads, reusing the current thread and blocking until
+        // all complete
+        threadSet.start(USE_CURRENT_THREAD);
+        threadSet.waitForCompletion();
+
+        for (int i = 0; i < 6; ++i) {
+            image[i]->unmap();
+        }
+
+        // Copy PBOs to GL texture faces
+        glBindTexture(sky->openGLTextureTarget(), sky->openGLID());
+        for (int i = 0; i < 6; ++i) {
+            glBindBuffer(GL_PIXEL_UNPACK_BUFFER, image[i]->pbo);
+            glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X_ARB + i, 0, GL_RGB8, w, w, 0, GL_RGB, GL_BYTE, 0);
+        }
+        glBindBuffer(GL_PIXEL_UNPACK_BUFFER, GL_NONE);
+        glBindTexture(sky->openGLTextureTarget(), GL_NONE);
+    }
+#endif
+
+    /*
+    Texture::Settings settings  = Texture::Settings::cubeMap();
+    settings.interpolateMode = Texture::BILINEAR_NO_MIPMAP;
+    Texture::Ref t = Texture::fromFile("D:/morgan/g3d/data/cubemap/sky_skylab_01/sky_skylab_01*.png", ImageFormat::AUTO(), Texture::DIM_CUBE_MAP, settings, skyprocess);
+    */
+    stopwatch.tock();
+    debugPrintf("Elapsed time: %fs\n", stopwatch.elapsedTime());
+
+
     // Called before the application loop beings.  Load data here and
     // not in the constructor so that common exceptions will be
     // automatically caught.
@@ -218,6 +308,7 @@ void App::onPose(Array<Surface::Ref>& surfaceArray, Array<Surface2D::Ref>& surfa
 
 
 void App::onGraphics3D(RenderDevice* rd, Array<Surface::Ref>& surface3D) {
+    Draw::skyBox(rd, sky, 1.0, 1.0);
 /*    if (m_scene->lighting()->environmentMapTexture.notNull()) {
         Draw::skyBox(rd, m_scene->lighting()->environmentMapTexture, m_scene->lighting()->environmentMapConstant);
     }
