cb9727ccc7df606dbafd1cb27261258f1b309988
corey_taylor <corey_taylor@ef0428c2-0c16-0410-9f3f-d710ca1f8061>
2011-06-02 03:57:15 +0000

    Concept BlockMemoryManager
    git-svn-id: https://g3d.svn.sourceforge.net/svnroot/g3d@678 ef0428c2-0c16-0410-9f3f-d710ca1f8061

--- G3D9/scratch/corey/main.cpp
+++ G3D9/scratch/corey/main.cpp
@@ -4,6 +4,76 @@
 
 //#pragma comment(lib, "irrKlang.lib") // link with irrKlang.dll
 
+class BlockMemoryManager : public MemoryManager {
+private:
+    size_t  mBlockSize;
+    size_t  mNumBlocks;
+    uint8*  mPool;
+    uint8*  mPoolEnd;
+
+    size_t  mNextFreeIndex;
+
+    int     mDebugAllocCount;
+
+public:
+    typedef ReferenceCountedPointer<class MemoryManager> Ref;
+
+    BlockMemoryManager(int blockSize, int numBlocks) 
+        : mBlockSize(blockSize)
+        , mNumBlocks(numBlocks)
+        , mNextFreeIndex(0)
+        , mDebugAllocCount(0) {
+        
+        debugAssert(blockSize >= 16);
+        mPool = static_cast<uint8*>(System::alignedMalloc(mBlockSize * mNumBlocks, 16));
+        mPoolEnd = mPool + (mNumBlocks * mBlockSize);
+
+        while (mNextFreeIndex < mNumBlocks) {
+            size_t* block = reinterpret_cast<size_t*>(mPool + (mNextFreeIndex * mBlockSize));
+
+            *block = mNextFreeIndex + 1;
+
+            ++mNextFreeIndex;
+        }
+
+        mNextFreeIndex = 0;
+    }
+
+    virtual ~BlockMemoryManager() {
+        debugAssert(mDebugAllocCount == 0);
+        System::alignedFree(mPool);
+    }
+
+    virtual void* alloc(size_t s) {
+        if (mNextFreeIndex == mNumBlocks || s > mBlockSize) {
+            return NULL;
+        } else {
+            ++mDebugAllocCount;
+
+            void* block = mPool + (mNextFreeIndex * mBlockSize);
+            mNextFreeIndex = *reinterpret_cast<size_t*>(block);
+            return block;
+        }
+    }
+
+    virtual void free(void* ptr) {
+        debugAssert(ptr >= mPool && ptr < mPoolEnd);
+        --mDebugAllocCount;
+
+        size_t* block = reinterpret_cast<size_t*>(ptr);
+        *block = mNextFreeIndex;
+
+        // the alternative implementation is to write the block address instead of index
+        int blockOffset = mPoolEnd - static_cast<uint8*>(ptr);
+        debugAssert(blockOffset % mBlockSize == 0);
+
+        mNextFreeIndex = blockOffset / mBlockSize;
+    }
+
+    virtual bool isThreadsafe() const {
+        return false;
+    }
+};
 
 class App : public GApp {
 public:
@@ -22,7 +92,13 @@ public:
     virtual void onNetwork();
     virtual void onSimulation(RealTime rdt, SimTime sdt, SimTime idt);
     virtual void onPose(Array<SurfaceRef>& posed3D, Array<Surface2DRef>& posed2D);
-    virtual void onGraphics(RenderDevice* rd, Array<SurfaceRef>& posed3D, Array<Surface2DRef>& posed2D);
+
+    // You can override onGraphics if you want more control over the rendering loop.
+    // virtual void onGraphics(RenderDevice* rd, Array<Surface::Ref>& posed3D, Array<Surface2D::Ref>& posed2D);
+
+    virtual void onGraphics3D(RenderDevice* rd, Array<Surface::Ref>& posed3D);
+    virtual void onGraphics2D(RenderDevice* rd, Array<Surface2D::Ref>& posed2D);
+
     virtual bool onEvent(const GEvent& e);
     virtual void onUserInput(UserInput* ui);
     virtual void onCleanup();
@@ -66,6 +142,8 @@ App::App(const GApp::Settings& settings) : GApp(settings) {
 
 
 void App::onInit() {
+    GApp::onInit();
+
     showRenderingStats = true;
 
     sky = Texture::fromFile(dataDir + "/cubemap/noonclouds/noonclouds_*.png", ImageFormat::AUTO(), Texture::DIM_CUBE_MAP_NPOT, Texture::Settings::cubeMap(), Texture::Preprocess::gamma(2.1f));
@@ -84,20 +162,41 @@ void App::onInit() {
 	// start the sound engine with default parameters
 	//irrklangDevice = irrklang::createIrrKlangDevice();
     //debugAssert(irrklangDevice);
+
+    void* blocks[1024] = {0};
+    BlockMemoryManager::Ref mm = new BlockMemoryManager(1024, 1024);
+    for (int blockIndex = 0; blockIndex < 1024; ++blockIndex)
+    {
+        blocks[blockIndex] = mm->alloc(1024);
+        debugAssert(blocks[blockIndex] != NULL);
+    }
+
+    debugAssert(mm->alloc(1024) == 0);
+
+    for (int blockIndex = 0; blockIndex < 1024; ++blockIndex)
+    {
+        mm->free(blocks[blockIndex]);
+    }
 }
 
 
 void App::onAI() {
+    GApp::onAI();
+
     // Add non-simulation game logic and AI code here
 }
 
 
 void App::onNetwork() {
+    GApp::onNetwork();
+
     // Poll net messages here
 }
 
 
 void App::onSimulation(RealTime rdt, SimTime sdt, SimTime idt) {
+    GApp::onSimulation(rdt, sdt, idt);
+
     // Add physical simulation here.  You can make your time
     // advancement based on any of the three arguments.
 
@@ -106,6 +205,10 @@ void App::onSimulation(RealTime rdt, SimTime sdt, SimTime idt) {
 
 
 bool App::onEvent(const GEvent& e) {
+    if (GApp::onEvent(e)) {
+        return true;
+    }
+
     // If you need to track individual UI events, manage them here.
     // Return true if you want to prevent other parts of the system
     // from observing this specific event.
@@ -119,34 +222,38 @@ bool App::onEvent(const GEvent& e) {
 
 
 void App::onUserInput(UserInput* ui) {
+    GApp::onUserInput(ui);
+
     // Add key handling here based on the keys currently held or
     // ones that changed in the last frame.
 }
 
 
-void App::onPose(Array<Surface::Ref>& surfaceArray, Array<Surface2D::Ref>& surface2DArray) {
-    (void)surface2DArray;
+void App::onPose(Array<Surface::Ref>& posed3D, Array<Surface2D::Ref>& posed2D) {
+    GApp::onPose(posed3D, posed2D);
+
     //modelPose.legsTime = realTime();
     //modelPose.torsoTime = realTime();
     //model->pose(surfaceArray, CoordinateFrame(), modelPose);
 }
 
 
-void App::onGraphics(RenderDevice* rd, Array<Surface::Ref>& surfaceArray, Array<Surface2DRef>& posed2D) {
-    rd->setProjectionAndCameraMatrix(defaultCamera);
-
-    rd->setColorClearValue(Color3(0.1f, 0.5f, 1.0f));
-    rd->clear(true, true, true);
-
+void App::onGraphics3D(RenderDevice* rd, Array<Surface::Ref>& posed3D) {
+    // Draw sky
     Draw::skyBox(rd, sky);
 
     // Render all objects (or, you can call Surface methods on the
     // elements of posed3D directly to customize rendering.  Pass a
     // ShadowMap as the final argument to create shadows.)
 
-    Surface::sortAndRender(rd, defaultCamera, surfaceArray, lighting);
-    
-    // Render 2D objects like Widgets
+    Surface::sortAndRender(rd, defaultCamera, posed3D, lighting);
+
+    // Call to make the GApp show the output of debugDraw
+    drawDebugShapes();
+}
+
+void App::onGraphics2D(RenderDevice* rd, Array<Surface2D::Ref>& posed2D) {
+    // Render 2D objects like Widgets.  These do not receive tone mapping or gamma correction
     Surface2D::sortAndRender(rd, posed2D);
 }
 
