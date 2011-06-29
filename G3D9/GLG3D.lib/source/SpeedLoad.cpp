27cb394bf862cc664141b0e6e62f5290bd56dde3
morgan3d <morgan3d@ef0428c2-0c16-0410-9f3f-d710ca1f8061>
2011-06-29 00:26:17 +0000

    git-svn-id: https://g3d.svn.sourceforge.net/svnroot/g3d@792 ef0428c2-0c16-0410-9f3f-d710ca1f8061

---
+++ G3D9/GLG3D.lib/source/SpeedLoad.cpp
@@ -0,0 +1,24 @@
+#include "G3D/BinaryInput.h"
+#include "G3D/BinaryOutput.h"
+#include "GLG3D/SpeedLoad.h"
+
+namespace G3D {
+namespace SpeedLoad { 
+
+void readHeader(BinaryInput& b, const std::string& expectedString) {
+
+    const std::string header = b.readString(SpeedLoad::HEADER_LENGTH);
+
+    (void)header;
+    alwaysAssertM(header == expectedString, 
+                  format("SpeedLoad expected to read chunk \"%s\" but found chunk \"%s\"",
+                         expectedString.c_str(), header.c_str()));
+}
+
+
+void writeHeader(BinaryOutput& b, const std::string& header) {
+    b.writeString(header, SpeedLoad::HEADER_LENGTH);
+}
+
+} // SpeedLoad
+} // G3D
