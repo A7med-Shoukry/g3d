0a7fb078c4a7d81fcec6d9fa0a22e17d2b4517f7
morgan3d <morgan3d@ef0428c2-0c16-0410-9f3f-d710ca1f8061>
2011-07-16 21:28:24 +0000

    git-svn-id: https://g3d.svn.sourceforge.net/svnroot/g3d@850 ef0428c2-0c16-0410-9f3f-d710ca1f8061

--- G3D9/scratch/morgan/App.cpp
+++ G3D9/scratch/morgan/App.cpp
@@ -4,6 +4,94 @@
 // Tells C++ to invoke command-line main() function even on OS X and Win32.
 G3D_START_AT_MAIN();
 
+
+/** Dumps the geometry and texture coordinates (no materials) to a
+    file.  Does not deal with nested parts */
+void convertToOBJFile(const std::string& srcFilename) {
+    const std::string dstFilename = FilePath::base(srcFilename) + ".obj";
+
+    FILE* file = FileSystem::fopen(dstFilename.c_str(), "wt");
+
+    ArticulatedModel::Ref m = ArticulatedModel::fromFile(srcFilename);
+    fprintf(file, "# %s\n\n", m->name.c_str());
+    for (int p = 0; p < m->partArray.size(); ++p) {
+        const ArticulatedModel::Part& part = m->partArray[p];
+
+
+        if (part.parent == -1) {
+            const CFrame& cframe = part.cframe;
+            
+            const bool hasTexCoords = part.texCoordArray.size() > 0;
+
+            // Number of vertices
+            const int N = part.geometry.vertexArray.size();
+
+            // Construct a legal part name
+            std::string name = "";
+            for (int i = 0; i < (int)part.name.size(); ++i) {
+                const char c = part.name[i];
+                if (isDigit(c) || isLetter(c)) {
+                    name += c;
+                } else {
+                    name += "_";
+                }
+            }
+
+            if (name == "") {
+                name = format("UnnamedPart%d", p);
+            }
+
+            // Part name
+            fprintf(file, "\ng %s \n", name.c_str());
+
+            // Write geometry
+            fprintf(file, "\n");
+            for (int v = 0; v < N; ++v) {
+                const Point3& vertex = cframe.pointToWorldSpace(part.geometry.vertexArray[v]);
+                fprintf(file, "v %g %g %g\n", vertex.x, vertex.y, vertex.z);
+            }
+            
+            if (hasTexCoords) {
+                fprintf(file, "\n");
+                for (int v = 0; v < N; ++v) {
+                    const Point2& texCoord = part.texCoordArray[v];
+                    fprintf(file, "vt %g %g\n", texCoord.x, texCoord.y);
+                }
+            }
+
+            fprintf(file, "\n");
+            for (int v = 0; v < N; ++v) {
+                const Vector3& normal = cframe.vectorToWorldSpace(part.geometry.normalArray[v]);
+                fprintf(file, "vn %g %g %g\n", normal.x, normal.y, normal.z);
+            }
+
+            // Triangle list
+            fprintf(file, "\n");
+            for (int t = 0; t < part.triList.size(); ++t) {
+                const ArticulatedModel::Part::TriList::Ref& triList = part.triList[t];
+                alwaysAssertM(triList->primitive == PrimitiveType::TRIANGLES, "Only triangle lists supported");
+                for (int i = 0; i < triList->indexArray.size(); i += 3) {
+                    fprintf(file, "f");
+                    for (int j = 0; j < 3; ++j) {
+                        // Indices are 1-based; negative values reference relative to the 
+                        // last vertex
+                        const int index = triList->indexArray[i + j] - N;
+                        if (hasTexCoords) {
+                            fprintf(file, " %d/%d/%d", index, index, index);
+                        } else {
+                            fprintf(file, " %d//%d", index, index);
+                        }
+                    }
+                    fprintf(file, "\n");
+                }
+            }
+        }
+    }
+    
+    FileSystem::fclose(file);
+}
+
+
 int main(int argc, const char* argv[]) {
     (void)argc; (void)argv;
     GApp::Settings settings(argc, argv);
@@ -19,6 +107,10 @@ int main(int argc, const char* argv[]) {
 
 App::App(const GApp::Settings& settings) : GApp(settings) {
     renderDevice->setColorClearValue(Color3::white());
+
+    convertToOBJFile("crate.ifs");
+    convertToOBJFile("teapot.ifs");
+    ::exit(0);
 }
 
 
