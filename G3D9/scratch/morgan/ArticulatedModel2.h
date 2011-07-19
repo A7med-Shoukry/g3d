4ca0693688fe6feb09eb587a52d9129e544bfa92
morgan3d <morgan3d@ef0428c2-0c16-0410-9f3f-d710ca1f8061>
2011-07-19 21:10:38 +0000

    git-svn-id: https://g3d.svn.sourceforge.net/svnroot/g3d@859 ef0428c2-0c16-0410-9f3f-d710ca1f8061

---
+++ G3D9/scratch/morgan/ArticulatedModel2.h
@@ -0,0 +1,54 @@
+#ifndef GLG3D_ArticulatedModel2_h
+#define GLG3D_ArticulatedModel2_h
+
+#include <G3D/G3DAll.h>
+
+class ArticulatedModel2 : public ReferenceCountedObject {
+public:
+
+    class Mesh {
+    public:
+
+    };
+
+    /** 
+     A set of meshes with a single reference frame, packed into a common vertex buffer.
+    */
+    class Part {
+    public:
+
+        const Part* parent() const;
+        const Array<Part*>& childArray() const;
+
+    };
+
+
+
+    /** Root parts */
+    const Array<Part*>& rootArray() const;
+
+    /** Create a new part.
+      \param parent        May be NULL, if a root part.
+      \param bodyToParent  Transformation from the part's reference frame to the parent's reference frame.
+      \return The new part, which has also been added to the model.
+      */
+    Part* addPart
+       (const std::string&              name, 
+        Part*                           parent, 
+        const CFrame&                   bodyToParent, 
+        Array<Point3>&                  vertex,
+        Array<Vector3>&                 normal,
+        Array<Point2>&                  texCoord,
+        Array<Vector4>&                 tangent,
+        Array<int>&                     triList,
+        Array<int>&                     oldToNew);
+
+    void addMesh
+        (const std::string&             name,
+         Part*                          part,
+         PrimitiveType                  type,
+         const Array<int>&              index,
+         const Material::Specification& materialSpec);
+};
+
+#endif
