38d0908fb77d07d05c07f0f6cbbd7a1211e78564
corey_taylor <corey_taylor@ef0428c2-0c16-0410-9f3f-d710ca1f8061>
2012-04-17 04:55:10 +0000

    Update ImageBuffer documentation
    git-svn-id: https://g3d.svn.sourceforge.net/svnroot/g3d@1295 ef0428c2-0c16-0410-9f3f-d710ca1f8061

--- G3D9/G3D.lib/include/G3D/ImageBuffer.h
+++ G3D9/G3D.lib/include/G3D/ImageBuffer.h
@@ -12,7 +12,13 @@
 
 namespace G3D {
 
+/**
+    \brief Provides a general container for transfering cpu pixel data
+ */
 class ImageBuffer : public ReferenceCountedObject {
+public:
+    typedef ReferenceCountedPointer<ImageBuffer> Ref;
+
 private:
     MemoryManager::Ref  m_memoryManager;
     void*               m_buffer;
@@ -31,30 +37,36 @@ private:
     void freeBuffer();
 
 public:
-    typedef ReferenceCountedPointer<ImageBuffer> Ref;
-
+    /** Creates an empty ImageBuffer. */
     static Ref create(MemoryManager::Ref memoryManager, const ImageFormat* format, int width, int height, int depth = 1, int rowAlignment = 1);
 
-    ~ImageBuffer();
+    virtual ~ImageBuffer();
 
     const ImageFormat* format() const   { return m_format; }
 
+    /** Returns entire size of pixel data in bytes. */
     int size() const                    { return m_height * m_depth * m_rowStride; }
+    /** Returns alignment of each row of pixel data in bytes. */
     int rowAlignment() const            { return m_rowAlignment; }
+    /** Returns size of each row of pixel data in bytes. */
     int stride() const                  { return m_rowStride; }
 
     int width() const                   { return m_width; }
     int height() const                  { return m_height; }
     int depth() const                   { return m_depth; }
 
+    /** Returns pointer to raw pixel data */
     void* buffer()                      { return m_buffer; }
+    /** Returns pointer to raw pixel data */
     const void* buffer() const          { return m_buffer; }
 
+    /** Return row to raw pixel data at start of row \a y of depth \a d. */
     void* row(int y, int d = 0) {
         debugAssert(y < m_height && d < m_depth);
         return static_cast<uint8*>(m_buffer) + (d * m_height * m_rowStride) + (y * m_rowStride); 
     }
 
+    /** Return row to raw pixel data at start of row \a y of depth \a d. */
     const void* row(int y, int d = 0) const {
         debugAssert(y < m_height && d < m_depth);
         return static_cast<uint8*>(m_buffer) + (d * m_height * m_rowStride) + (y * m_rowStride); 
