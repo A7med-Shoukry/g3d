aea838912b0ced7f173cfc09ec15172508189336
mikemx7f <mikemx7f@ef0428c2-0c16-0410-9f3f-d710ca1f8061>
2012-06-16 22:03:45 +0000

    git-svn-id: https://g3d.svn.sourceforge.net/svnroot/g3d@1332 ef0428c2-0c16-0410-9f3f-d710ca1f8061

--- G3D9/GLG3D.lib/include/GLG3D/Shader2.h
+++ G3D9/GLG3D.lib/include/GLG3D/Shader2.h
@@ -4,11 +4,11 @@
  \maintainer Morgan McGuire, Michael Mara http://graphics.cs.williams.edu
  
  \created 2012-06-13
- \edited 2012-06-13
+ \edited 2012-06-16
  */
 
-#ifndef G3D_Shader2_h
-#define G3D_Shader2_h
+#ifndef GLG3D_Shader2_h
+#define GLG3D_Shader2_h
 
 #include "G3D/Matrix2.h"
 #include "G3D/Matrix3.h"
@@ -23,6 +23,7 @@
 #include "G3D/ReferenceCount.h"
 #include "GLG3D/VertexRange.h"
 #include "GLG3D/Texture.h"
+#include "GLG3D/Args.h"
 #include "G3D/SmallArray.h"
 #include <string>
 
@@ -71,12 +72,43 @@ Make G3D assume programmable pipeline and not retain fixed-function state by def
 
 
 
+    Commonly initialized with the idiom:
+
+    \code
+    m_toonShader = Shader2::fromFiles("toon.vrt", "toon.pix");
+    \endcode
+
+    or
+
+    \code
+    Shader2::Specification toonShaderSpec;
+    toonShaderSpec[Shader2::VERTEX] = STR( 
+    void main() {
+        gl_Position = ...
+    });
+
+    m_toonShader = Shader2::create(toonShaderSpec);
+    \endcode
+
  */ 
  
 class Shader2  : public ReferenceCountedObject {
 public:
     typedef ReferenceCountedPointer<Shader2>   Ref;
-
+            
+    enum ShaderStage {
+        VERTEX, 
+        
+        /** In DirectX, this is called the Hull Shader */
+        TESSELLATION_CONTROL, 
+        
+        /** In DirectX, this is called the Domain Shader */
+        TESSELLATION_EVAL, 
+        
+        GEOMETRY, 
+        
+        PIXEL
+    };
 
     enum SourceType {FILE, STRING};
 
@@ -85,136 +117,47 @@ public:
     protected:
         SourceType type;
         std::string val;
+
     public:
 
-        Source(SourceType t = STRING, const std::string& value = "");
+        Source();
 
+        Source(SourceType t, const std::string& value);
+
+        /** Throws an error at runtime if the source string appears to be a filename instead of GLSL code */
+        Source(const std::string& sourceString);
     };
 
-    /* 
-    Shader2::Specification toonShaderSpec;
-    toonShaderSpec.vertex = Shader2::Source(Shader2::FILE, "toon.vrt");
-    toonShaderSpec.pixel = Shader2::Source(Shader2::FILE, "toon.pix");
-    
-    m_toonShader = Shader2::create(toonShaderSpec);
-
-    
-    
-    */
     class Specification {
+    protected:
+        Source shaderStage[5];
+
     public:
         Specification(const Any& any);
         Specification();
 
-        Source vertex;
+        /** appropruiate documentation 
+    
+        vrt or vtx
+        evl or hul
+        ctl or dom
+        geo
+        pix or frg
+        */
+        Specification(
+            const std::string& f0, 
+            const std::string& f1 = "", 
+            const std::string& f2 = "", 
+            const std::string& f3 = "", 
+            const std::string& f4 = "");
 
-        /* In DirectX, this is called the Hull Shader */
-        Source tessellationControl;
+        Source& operator[](ShaderStage s);
+        const Source& operator[](ShaderStage s) const;
 
-        /* In DirectX, this is called the Domain Shader */
-        Source tessellationEvaluation;
-
-        Source geometry;
-        Source pixel;
     };
 
     
-    /** All arguments */
-    class ArgGroup {
-
-        /* 8-byte storage used for all argument types */
-        union Scalar {
-            float  f;
-            int32  i;
-            uint32 ui;
-            bool   b;
-            double d;
-        }; 
-
-        /* This contains the value of a uniform or macro argument passed to shader and its type 
-           Macro variables can only be a subset of the possible values */
-        class Arg {
-        public:
-            GLenum                  type;
-
-            // Empty unless this argument is an OpenGL Sampler
-            Texture::Ref            texture;
-            SmallArray<Scalar, 4>   value;
-            std::string toString() const;
-
-        };
-
-        typedef Table<std::string, Arg> ArgTable;
-       
-    private:
-        
-        std::string                                 m_preamble;
-        ArgTable                                    m_macroArgs;
-        ArgTable                                    m_uniformArgs;
-        
-        Table<std::string, const VertexRange>       m_streamArgs;
-        int                                         m_numInstances;
-
-        /** If empty, sequential indicies will be used */
-        VertexRange                                 m_indexArray;
-
-    public:
-
-        PrimitiveType geometryInput;
-        PrimitiveType geometryOutput;
-
-        void setIndexArray(const VertexRange indArray);
-
-        /** Arbitrary string to append to beginning of the shader */
-        void setPreamble(const std::string& preamble);
-        void setStream(const std::string& name, const VertexRange& val);
-        void setStream(GLenum name, const VertexRange& val);
-
-        void setNumInstances(int num);
-
-        // Supports bool, int, float, vec234, mat234
-        void setMacro(const std::string& name, bool             val);
-        void setMacro(const std::string& name, int              val);
-        void setMacro(const std::string& name, uint32           val);
-        void setMacro(const std::string& name, double           val);
-        void setMacro(const std::string& name, float            val);
-        void setMacro(const std::string& name, const Vector2&   val);
-        void setMacro(const std::string& name, const Vector3&   val);
-        void setMacro(const std::string& name, const Vector4&   val);
-        /** Becomes float in GLSL */
-        void setMacro(const std::string& name, const Color1&    val);
-        void setMacro(const std::string& name, const Color3&    val);
-        void setMacro(const std::string& name, const Color4&    val);
-        void setMacro(const std::string& name, const Matrix2&   val);
-        void setMacro(const std::string& name, const Matrix3&   val);
-        void setMacro(const std::string& name, const Matrix4&   val);
-
-        /** Becomes mat3x4 in GLSL */
-        void setMacro(const std::string& name, const CoordinateFrame&   val);        
-
-        void setUniform(const std::string& name, bool             val, bool optional = false);
-        void setUniform(const std::string& name, int              val);
-        void setUniform(const std::string& name, float            val);
-        void setUniform(const std::string& name, uint32           val);
-        void setUniform(const std::string& name, double           val);
-        void setUniform(const std::string& name, const Vector2&   val);
-        void setUniform(const std::string& name, const Vector3&   val);
-        void setUniform(const std::string& name, const Vector4&   val);
-
-        /** Becomes float in GLSL */
-        void setUniform(const std::string& name, const Color1&    val);
-        void setUniform(const std::string& name, const Color3&    val);
-        void setUniform(const std::string& name, const Color4&    val);
-        void setUniform(const std::string& name, const Matrix2&   val);
-        void setUniform(const std::string& name, const Matrix3&   val);
-        void setUniform(const std::string& name, const Matrix4&   val);
-
-        // Look at Shader to see how this is handled
-        void setUniform(const std::string& name, const CoordinateFrame&   val);
-
-        void setUniform(const std::string& name, const Texture::Ref& val);
-
-    }; // class ArgGroup
+    
 
 protected:
 
@@ -242,7 +185,7 @@ protected:
 
 public:
 
-    ArgGroup args;
+    Args args;
 
     enum FailureBehavior {
         /** Throw an exception on compilation failure */
@@ -264,6 +207,14 @@ public:
 
     /** Loads and compiles the shader */
     static Ref create(const Specification& s);
+
+    /** \copydoc Shader2::Source::Source() */
+    static Ref fromFiles(
+        const std::string& f0, 
+        const std::string& f1 = "", 
+        const std::string& f2 = "", 
+        const std::string& f3 = "", 
+        const std::string& f4 = "");
 
     /** Reload this shader from the files on disk, if it was loaded from disk. */
     void reload();
